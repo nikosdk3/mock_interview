2fd539d18cc21397001fe767ab160e708b0cdf26
nikosdk3 <nikomvp03@gmail.com>
2025-09-01 20:24:10 -0500

    Interview page and mock interview cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 
 const Page = () => {
   return (
-    <div>Home Page</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex max-w-lg flex-col gap-6">
+          <h2>Prepare for your interview with the help of AI</h2>
+          <p className="text-lg">
+            Practice real interview questions & get feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
 
-export default Page
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="mt-8 flex flex-col gap-6">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+        </div>
+      </section>
+
+      <section className="mt-8 flex flex-col gap-6">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+          {/* <p>There are no interviews available.</p> */}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
---
+++ components/InterviewCard.tsx
@@ -0,0 +1,80 @@
+import DisplayTechIcons from "@/components/DisplayTechIcons";
+import { Button } from "@/components/ui/button";
+import { getRandomInterviewCover } from "@/lib/utils";
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const InterviewCard = ({
+  interviewId,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now(),
+  ).format("MMM D, YYYY");
+  return (
+    <div className="card-border min-h-96 w-[360px] max-sm:w-full">
+      <div className="card-interview">
+        <div>
+          <div className="bg-light-600 absolute top-0 right-0 w-fit rounded-bl-lg px-4 py-2">
+            <p className="badge-text">{normalizedType}</p>
+          </div>
+
+          <Image
+            src={getRandomInterviewCover()}
+            alt="cover image"
+            width={90}
+            height={90}
+            className="object-fit size-[90px] rounded-full"
+          />
+
+          <h3 className="mt-5 capitalize">{role} Interview</h3>
+
+          <div className="mt-3 flex flex-row gap-5">
+            <div className="text-light-400 flex flex-row gap-2">
+              <Image
+                src="/calendar.svg"
+                alt="calendar"
+                width={22}
+                height={22}
+              />
+              <p>{formattedDate}</p>
+            </div>
+
+            <div className="flex flex-row items-center gap-2">
+              <Image src="/star.svg" alt="star" width={22} height={22} />
+              <p>{feedback?.totalScore || "---"}/100</p>
+            </div>
+          </div>
+          <p className="mt-5 line-clamp-2">
+            {feedback?.finalAssessment ||
+              "You haven't taken the interview yet. Take it now to improve your skills"}
+          </p>
+        </div>
+        <div className="flex flex-row justify-between">
+          <DisplayTechIcons techStack={techstack} />
+          <Button className="btn-primary">
+            <Link
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
+            >
+              {feedback ? "Check feedback" : "View interview"}
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InterviewCard;
